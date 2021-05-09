958b51dc230ac320c1bbd3549412728da4b8312f
Manoel Castro <manoelcastroadm@gmail.com>
2021-05-09 20:50:39 -0300

    Desafio Completo

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -2,14 +2,22 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+  user_id: string | string[];
 }
 
 class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (user) {
+      if (!user.admin) {
+        throw new Error("You don't is a admin");
+      }
+      return this.usersRepository.list();
+    }
+    throw new Error("This user is not authorized");
   }
 }
 
